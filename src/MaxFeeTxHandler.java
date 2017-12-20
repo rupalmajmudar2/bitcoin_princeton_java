208880b23bcdab485f34f8222bad4c78f56e6333
Rupal TestLocal <rupal@merasport.com>
2017-12-20 16:33:38 +0530

    v0

---
+++ src/MaxFeeTxHandler.java
@@ -0,0 +1,12 @@
+
+public class MaxFeeTxHandler extends TxHandler {
+
+	public MaxFeeTxHandler(UTXOPool utxoPool) {
+		super(utxoPool);
+	}
+
+    public Transaction[] handleTxs(Transaction[] possibleTxs) {
+    	
+    	return null;
+    }
+}
